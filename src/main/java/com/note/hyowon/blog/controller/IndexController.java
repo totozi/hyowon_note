e76133665db0c528255dcadd3bf663988a1b9f92
hyowon <skgydnjs@gmail.com>
2022-11-24 02:25:22 +0900

    replaced for new project

---
+++ src/main/java/com/note/hyowon/blog/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.note.hyowon.blog.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@Log4j2
+public class IndexController {
+
+    @GetMapping
+    public String home(){
+        log.info("home..................");
+        return "/blog/index";
+    }
+}
