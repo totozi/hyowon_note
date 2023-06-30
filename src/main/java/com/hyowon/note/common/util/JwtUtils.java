31f58b3a4fb70ca98b7f8a1faa45ee57905f04fc
Hyowon Na <87255155+totozi@users.noreply.github.com>
2023-06-30 14:45:32 +0900

    properties 파일 자바로 읽어서 키 생성

--- src/main/java/com/hyowon/note/common/util/JwtUtils.java
+++ src/main/java/com/hyowon/note/common/util/JwtUtils.java
@@ -17,12 +17,46 @@ public class JwtUtils {
 
 
 //    @Value("#{oauth['jwt.secretKey']}")
-    private String secretKeyStr = "skgydnjs123456skgydnjs123456skgydnjs123456"; // TODO 정상적인 문구로 바꾸거나 외부에서 가져오기
+    //private String secretKeyStr = "skgydnjs123456skgydnjs123456skgydnjs123456"; // TODO 정상적인 문구로 바꾸거나 외부에서 가져오기
 
     // 암복호화에 사용되는 키
-    private Key secretKey = new SecretKeySpec(secretKeyStr.getBytes(),  SignatureAlgorithm.HS256.getJcaName());
+    //private Key secretKey = new SecretKeySpec(secretKeyStr.getBytes(),  SignatureAlgorithm.HS256.getJcaName());
 
     public String createToken(String email) {
+
+        // 키 생성을 위한 String값 가져오기
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = null;
+
+        private String secretKeyStr = "";
+
+        private Key secretKey;
+
+        try {
+            // properties 파일 경로
+            String filePath = "classpath:application.properties";
+            
+            // 파일을 FileInputStream을 통해 읽어옴
+            fileInputStream = new FileInputStream(filePath);
+            
+            // Properties 객체에 파일 내용을 로드
+            properties.load(fileInputStream);
+            
+            // 특정 키에 대한 값을 가져옴
+            secretKeyStr = properties.getProperty("jwt.secretKey");
+            
+            // 값을 출력
+            System.out.println(secretKeyStr);
+
+            // 암복호화에 사용되는 키
+            secretKey = new SecretKeySpec(secretKeyStr.getBytes(),  SignatureAlgorithm.HS256.getJcaName());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            // 오류 리턴
+        }
+        
+
         Claims claims = Jwts.claims(); // 나중에 서버에서 파싱해서 볼 데이터
         claims.put("email", email);
         Date now = new Date();
